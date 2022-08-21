7409e6a528be747bc82e340b7ee937c77ca5bf3b
Bryant Yang <chiru127@hotmail.com>
2022-08-21 15:31:03 -0700

    Initial commit

---
+++ src/main/java/com/byang24/technology/springcloudeurekaclient/SpringCloudEurekaClientApplication.java
@@ -0,0 +1,33 @@
+package com.byang24.technology.springcloudeurekaclient;
+
+import com.byang24.technology.springcloudeurekaclient.controllers.GreetingController;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringCloudEurekaClientApplication implements GreetingController {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaClientApplication.class, args);
+    }
+
+    @Override
+    public String greeting() {
+        return String.format(
+                "Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+    }
+
+}
